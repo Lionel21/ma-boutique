fdf487b36c2de3b36f41712608dfcc0f515c8a2f
Lionel Delamare <lionel.delamare>
2020-11-28 12:29:40 +0100

    User entity mapped in EasyAdmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -24,9 +25,10 @@ class DashboardController extends AbstractDashboardController
             ->setTitle('Ma Boutique');
     }
 
+    // Méthode pour configurer notre menu
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+         yield MenuItem::linkToCrud('Utilisateur', 'fa fa-user', User::class);
     }
 }
