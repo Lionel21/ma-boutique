6a8fda9ea5bbf64228383f9be83b9159b1373ee7
Lionel Delamare <lionel.delamare>
2020-11-28 10:03:20 +0100

    form created

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,12 @@ class AccountPasswordController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('account/password.html.twig');
+        // J'appelle l'objet User qui est connecté (je n'en crée pas un nouveau)
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordType::class, $user);
+
+        return $this->render('account/password.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
