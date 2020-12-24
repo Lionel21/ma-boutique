24351a519ae2a55267678cba2d41dd3cb848365d
Lionel Delamare <lionel.delamare>
2020-12-24 16:15:31 +0100

    display front cart and calculations

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,20 +3,42 @@
 namespace App\Controller;
 
 use App\Controller\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/mon-panier", name="cart")
      */
     public function index(Cart $cart): Response
     {
-        // Récupération du panier
-        dd($cart->get());
-        return $this->render('cart/index.html.twig');
+        $cartComplete = [];
+
+        // Itération pour avoir accès à tout mon objet Product
+        foreach ($cart->get() as $id => $quantity) {
+            $cartComplete[] = [
+                // Appel de l'objet Product
+                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
+                'quantity' => $quantity
+            ];
+        }
+
+//        dd($cartComplete);
+
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cartComplete,
+        ]);
     }
 
     /**
