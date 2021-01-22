19971c96b6482ccb12f6dd719764871a0762adf9
Lionel Delamare <lionel.delamare>
2021-01-22 11:32:40 +0100

    StripeController created

---
+++ src/Controller/StripeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StripeController extends AbstractController
+{
+    /**
+     * @Route("/commande/create-session", name="stripe_create_session")
+     */
+    public function index(): Response
+    {
+
+    }
+}
