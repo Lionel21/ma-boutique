d8add19f34f2ae1c18bf097a3c984f1852a7f9a4
Lionel Delamare <lionel.delamare>
2021-01-23 15:08:26 +0100

    New Repository function for orders

--- src/Controller/AccountOrderController.php
+++ src/Controller/AccountOrderController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountOrderController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/compter/mes-commandes", name="account_order")
      * Affichage des commandes dans l'ineterface utilisateur
      */
     public function index(): Response
     {
+        $orders = $this->entityManager->getRepository(Order::class)->findSuccessOrders($this->getUser());
+
+        dd($orders);
+
         return $this->render('account/order.html.twig');
     }
 }
