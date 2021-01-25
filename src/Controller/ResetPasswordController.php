4f0fa4148bf813177a1baa6bcf4df5476be2a6ca
Lionel Delamare <lionel.delamare>
2021-01-25 13:26:21 +0100

    function reset password

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -2,17 +2,75 @@
 
 namespace App\Controller;
 
+use App\Controller\Classe\Mailjet;
+use App\Entity\ResetPassword;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ResetPasswordController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/mot-de-passe-oublie", name="reset_password")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // Si l'utilisateur est connecté => redirection vers la page d'accueil
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
+        // Je récupère le name de l'input "inputEmail"
+        if ($request->get('email')) {
+            // Vérification si l'utilisateur existe
+            $user = $this->entityManager->getRepository(User::class)->findOneByEmail($request->get('email'));
+
+            // Si l'utilisateur existe =>
+            if ($user) {
+                // TODO : enregistrement en base de données la demande de reset_password avec user, token et createdAt
+                $reset_password = new ResetPassword();
+                $reset_password->setUser($user);
+                $reset_password->setToken(uniqid());
+                $reset_password->setCreatedAt(new \DateTime());
+                $this->entityManager->persist($reset_password);
+                $this->entityManager->flush();
+
+                // TODO : envoyer un email à l'utilisateur avec un lien lui permettant de mettre à jour son mot de passe
+
+                // On génère un URL pour la réinitialisation du mot de passe grâce à la méthode generateUrl
+                // 2 params : nom de la route, le token (tableau)
+                $url = $this->generateUrl('update_password',  [
+                    'token' => $reset_password->getToken()
+                ]);
+
+                $content = "Bonjour " . $user->getFirstname() . ' ' . $user->getLastname() . "<br /> Vous avez demandé de réinitialiser votre mot de passe sur le site Ma Boutique <br /> <br />";
+                $content .= "Merci de bien vouloir cliquer sur le lien suivant pour <a href='".$url."'> mettre à jour votre mot de passe.</a>";
+
+                $mail = new Mailjet();
+
+
+                $mail->send($user->getEmail(), $user->getFirstname() . ' ' . $user->getLastname(), 'Réinitialiser votre mot de passe sur Ma Boutique', $content);
+            }
+        }
+
         return $this->render('reset_password/index.html.twig');
+    }
+
+    /**
+     * @Route("/modifier-mon-mot-de-passe/{token}", name="update_password")
+     */
+    public function update(Request $request, $token)
+    {
+        dd($token);
     }
 }
